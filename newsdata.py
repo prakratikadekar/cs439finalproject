ca5d397ada160485c4dd1c8712d9b3dc89eaed28
prakratikadekar <prakrati.kadekar@gmail.com>
2025-12-07 14:49:25 -0500

    cleaned up news file with functions

--- newsdata.py
+++ newsdata.py
@@ -2,95 +2,106 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 import re
 import numpy as np
-# import kaggle
+import kaggle
 import time 
 
+
+def download_data():
+        # could not push this in github so this is how we can get the filtered dataset
+        kaggle.api.dataset_download_files (
+            'prakratikadekar/guardian-filtered',
+            path='data/',
+            unzip=True
+        )
+
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.max_colwidth', None)
+
+        return 'data/guardian_filtered.csv'
+
+
+
 model = SentenceTransformer('all-MiniLM-L6-v2')
-
-
-
-# sample user input
+df = pd.read_csv(download_data())
 
 
 
 # ----------- STEP 1: Filter dataset for just political and computer science/technology topics ---------------------------------------
 
-
-# df = pd.read_csv('guardian_articles.csv')
-
-# sections_to_keep = [
-#     "US news",
-#     "World news",
-#     "News",
-#     "Australia news",
-#     "UK news",
-#     "Politics",
-#     "Technology"
-# ]
-
-# filtered_news = df[df['sectionName'].isin(sections_to_keep)]
-# filtered_news = filtered_news.reset_index(drop=True)
-
-# filtered_news.to_csv('guardian_filtered.csv', index=False)
-
-# filtered dataset that was obtained from original 
-# df = pd.read_csv('https://www.kaggle.com/datasets/prakratikadekar/guardian-filtered', sep='\t')
-
-# kaggle.api.dataset_download_files(
-#     'prakratikadekar/guardian-filtered',
-#     path='data/',
-#     unzip=True
-# )
-
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', None)
-
-# ----------- STEP 2: Read dataset and convert the data columns of title and body content into strings ---------------------------------------
+# original df: https://www.kaggle.com/datasets/adityakharosekar2/guardian-news-articles aka data/guardian_articles.csv
+# filtered df: data/guardian_filtered.csv 
 
 
-df = pd.read_csv('data/guardian_filtered.csv', encoding='utf-8')
-
-# print('columns: ', df.columns)
-# print('dataset type: ', df['bodyContent'].dtype)
-# print('first 5 entries: ', df.head())
-
-# object -> string
-# df['bodyContent'] = df['bodyContent'].astype(str)
-# df['webTitle'] = df['webTitle'].astype(str)
+def remove_whitespace(col_data):
+    col_data = re.sub(r'<.*?>', '', col_data) # removes HTML
+    col_data = re.sub(r'\s+', ' ', col_data) # removes extra whitespace
+    return col_data.lower()
+    # print('cleaned data')
 
 
-# ----------- STEP 3: Clean dataset to get rid of extra HTML from kaggle + extra whitespace  ---------------------------------------
 
-# def clean_datacol(col_data):
-#     col_data = re.sub(r'<.*?>', '', col_data) # removes HTML
-#     col_data = re.sub(r'\s+', ' ', col_data) # removes extra whitespace
-#     return col_data.lower()
-# print('cleaned data')
+def filter_df():
+
+    df = 'data/guardian_articles.csv'
+
+    sections_to_keep = [
+        "US news",
+        "World news",
+        "News",
+        "Australia news",
+        "UK news",
+        "Politics",
+        "Technology"
+    ]
+
+    filtered_news = df[df['cleaned_section'].isin(sections_to_keep)]
+    filtered_news = filtered_news.reset_index(drop=True)
+
+    filtered_news.to_csv('guardian_filtered.csv', index=False)
+
+    # filtered dataset that was obtained from original 
 
 
-# #bodyContent in dataset is the article content (title and everything else kept the same)
-# df['cleaned_bodyContent'] = df['bodyContent'].apply(clean_datacol)
-# df['cleaned_title'] = df['webTitle'].apply(clean_datacol)
-# print('applied cleaning')
 
-# article_titles = df['cleaned_title'].tolist()
-# articles_body_content = df['cleaned_bodyContent'].tolist()
 
-# ----------- STEP 4: Use the Sentence Transformer model to encode both the titles and body content of articles with sentiment values (384 dimensional vector). This is a pretrained model  ---------------------------------------
+    # ----------- STEP 2: Read dataset and convert the data columns of title and body content into strings ---------------------------------------
+
+def clean_data():
+         
+    df['bodyContent'] = df['bodyContent'].astype(str)
+    df['webTitle'] = df['webTitle'].astype(str)
+    df['sectionName'] = df['sectionName'].astype(str)
+
+    # ----------- STEP 3: Clean dataset to get rid of extra HTML from kaggle + extra whitespace  ---------------------------------------
+
+
+    df['cleaned_bodyContent'] = df['bodyContent'].apply(remove_whitespace)
+    df['cleaned_title'] = df['webTitle'].apply(remove_whitespace)
+
+    return df['cleaned_title'], df['cleaned_bodyContent']
+
+
+
+    
+#    ----------- STEP 4: Use the Sentence Transformer model to encode both the titles and body content of articles with sentiment values (384 dimensional vector). This is a pretrained model  ---------------------------------------
 
 # model documentation: https://sbert.net/examples/sentence_transformer/applications/computing-embeddings/README.html
 
-# news_content = df['cleaned_title'] + " " + df['cleaned_bodyContent']
-# news_embeddings = model.encode(news_content.tolist(), batch_size=100)
-# print('model has been applied')
+def sentence_transformer():
+    clean_titles, clean_body_content = clean_data()
+    news_content = clean_titles + " " + clean_body_content
+    news_embeddings = model.encode(news_content.tolist(), batch_size=100)
+    print('model has been applied')
 
 
-# ----------- STEP 5: After running above code once, saved it to an npy file so we do not need to rerun again - these are our news embeddings  ---------------------------------------
+    # ----------- STEP 5: After running above code once, saved it to an npy file so we do not need to rerun again - these are our news embeddings  ---------------------------------------
 
-# np.save('news_embeddings.npy', news_embeddings)
+    np.save('news_embeddings.npy', news_embeddings)
+    print('shape of news embeddings', news_embeddings.shape)
 
-# print('shape of news embeddings', news_embeddings.shape)
+
+
 
 # ----------- STEP 6: Cosine similarity: use the embeddings of user input and news data to find which articles are similar to  ---------------------------------------
 # ----------- STEP 7: Gather highest 3 cosine similarities of input and embedding data ---------------------------------------
@@ -105,7 +116,7 @@ def get_top_news_matches(user_input):
     user_input_embedding = np.atleast_2d(user_input_embedding)
     user_input_normalized = user_input_embedding / np.linalg.norm(user_input_embedding, axis=1, keepdims=True)
 
-    news_embeddings_normalized = news_embeddings / np.linalg.norm(news_embeddings, axis=1, keepdims=True) # column -> 1 dimension across all articles (rows)
+    news_embeddings_normalized = news_embeddings / np.linalg.norm(news_embeddings, axis=1, keepdims=True) # 1 dimension across all articles (rows)
 
     similarities = np.dot(user_input_normalized, news_embeddings_normalized.T)
     
@@ -115,7 +126,7 @@ def get_top_news_matches(user_input):
     # print('best matches shape: ', best_matches.shape)
 
     top_similarities = similarities[0][best_matches]
-    print(top_similarities)
+    # print(top_similarities)
     valid_matches = []
 
     for match, similarity in zip(best_matches, top_similarities):
@@ -131,6 +142,9 @@ def get_top_news_matches(user_input):
 
     final_recommendation = []
 
+
+    # this is how it best displays on the website
+
     for _, row in recommended_guardian_articles.iterrows():
         article_dict = {
             'title': str(row['webTitle']) if pd.notna(row['webTitle']) else 'Unknown Title',
@@ -144,7 +158,7 @@ def get_top_news_matches(user_input):
 
 
 # main:
-# get_top_news_matches('tariffs')
+# get_top_news_matches('trump')
  
 
 
@@ -158,11 +172,6 @@ def get_top_news_matches(user_input):
 
 
 # ----------------------- ARCHIVE CODE -------------------------------------------------------
-
-
-
-
-
 
 
 # similarities = cosine_similarity([user_input_embedding], article_embeddings)
@@ -183,66 +192,3 @@ def get_top_news_matches(user_input):
 
 # title_normalized = np.linalg.norm(title_embeddings, axis=1, keepdims=True)
 # body_content_normalized = np.linalg.norm(body_content_embeddings, axis=1, keepdims=True)
-
-
-# df['embedded_title'] = df['cleaned_title'].apply(lambda x: model.encode(x))
-# df['embedded_bodyContent'] = df['cleaned_bodyContent'].apply(lambda x: model.encode(x))
-
-
-# df_first10000 = df.loc[:9999]
-
-# df_first10['embedded_title'] = df_first10['cleaned_title'].apply(lambda x: model.encode(x))
-# print('apply embeddings to titles')
-
-# title_embeddings = np.vstack(df_first10['embedded_title'].values)
-# similarities = cosine_similarity([user_input_embedding], title_embeddings)
-# print('used cosine similarities with body content')
-
-
-
-# import kagglehub
-
-
-# import pandas as pd
-# pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
-# pd.set_option('display.max_rows', None)
-
-# df = pd.read_csv('guardian_articles.csv')
-
-# # print(df.head()) # print first 5 rows
-
-
-# sections_to_keep = [
-#     "US news",
-#     "World news",
-#     "News",
-#     "Australia news",
-#     "UK news",
-#     "Politics",
-#     "Technology"
-# ]
-
-# filtered_news = df[df['sectionName'].isin(sections_to_keep)]
-# filtered_news = filtered_news.reset_index(drop=True)
-
-# filtered_news.to_csv('guardian_filtered.csv', index=False)
-
-# print('Filtered dataset: ', len(filtered_news))
-
-# path = kagglehub.dataset_download("prakratikadekar/guardian-filtered")
-
-
-
-# print("USERNAME =", os.getenv("KAGGLE_USERNAME"))
-# print("KEY =", os.getenv("KAGGLE_KEY"))
-
-# try:
-#     kaggle.api.set_config_value("username", os.getenv("KAGGLE_USERNAME"))
-#     kaggle.api.set_config_value("key", os.getenv("KAGGLE_KEY"))
-#     kaggle.api.authenticate()
-#     print("Auth OK")
-# except Exception as e:
-#     print("Auth FAIL:", e)
-
-# print(kaggle.api.dataset_list(user='prakratikadekar'))
