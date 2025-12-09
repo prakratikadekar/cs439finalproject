0acb0f2e13363031ced4017f3516887887a71491
prakratikadekar <prakrati.kadekar@gmail.com>
2025-12-09 17:30:00 -0500

    removed old code from youtube

--- youtubesearch.py
+++ youtubesearch.py
@@ -1,130 +1,13 @@
-# from dotenv import load_dotenv
-# import numpy as np
-# import os
-# from googleapiclient.discovery import build
-
-# print('before import sentence')
-# from sentence_transformers import SentenceTransformer
-
-# print('before model')
-# model = SentenceTransformer('all-MiniLM-L6-v2')
-# print('after model')
-
-
-# # search and then puts the results into a list
-# def search_videos(input):
-#     print(" ---- IN YOUTUBE ----")
-#     # load api
-#     load_dotenv('api.env')
-#     youtube_api = os.getenv('YOUTUBE_API_KEY')
-#     print("we got the api key")
-
-#     # https://developers.google.com/youtube/v3/docs
-#     youtube = build('youtube', 'v3', developerKey=youtube_api)
-
-#     video_categories = ['25', '28'] # according to api these numbers indicate politics and tech/cs specifically
-
-#     for category in video_categories:
-#         request = youtube.search().list(
-#             q=input, # what you search for
-#             part='snippet', # title, descp, thumbnails, channel, publish date
-#             type='video',
-#             maxResults=5, # extended to 5 and remove youtube shorts
-#             videoCategoryId = category
-#         )
-#         response = request.execute()
-
-#     orig_results = []
-#     print("got response from youtube")
-
-#     for result in response['items']:
-#             if len(orig_results) == 3:
-#                 break
-
-#             # checking if this is a yt short -> if yes discard
-#             if result['id']['kind'] != 'youtube#video':
-#                 continue
-#             title = result['snippet']['title']
-#             video_id = result['id']['videoId']
-#             channel = result['snippet']['channelTitle']
-#             description = result['snippet']['description']
-
-#             # file.write(f'{title} - youtube.com/watch?v={video_id}\n')
-#             video_link = f'https://youtube.com/watch?v={video_id}'
-
-#             video_dict = {
-#                 'title': title,
-#                 'channel': channel,
-#                 'description': description,
-#                 'url': video_link
-#             }
-
-#             orig_results.append(video_dict)
-#             print('added video: ', title)
-    
-    
-#     if not orig_results:
-#         return []        
-    
-    
-#     input_embedding = model.encode(input)
-
-#     filtered_results = []
-
-#     for video in orig_results: 
-#         video_text = video['title']
-#         embedded_video_text = model.encode(video_text)
-
-#         input_embedding = np.atleast_2d(input_embedding)
-#         embedded_video_text = np.atleast_2d(embedded_video_text)
-        
-#         user_input_normalized = input_embedding / np.linalg.norm(input_embedding, axis=1, keepdims=True)
-#         embedded_video_text = embedded_video_text / np.linalg.norm(embedded_video_text, axis=1, keepdims=True) # 1 dimension across all articles (rows)
-
-    
-#         similarities = np.dot(user_input_normalized, embedded_video_text.T)
-
-
-#         print('video similarity: ', similarities)
-#         # this value generally worked the best
-#         if similarities >= 0.4:
-#             video['similarity'] = similarities
-#             filtered_results.append(video)
-
-
-#     filtered_results.sort(key=lambda vid: vid['similarity'], reverse=True)
-    
-#     filtered_top3_results = filtered_results[:3]
-
-#     top_filtered_results = {
-#         str(index): video for index, video in enumerate(filtered_top3_results)
-#     }
-   
-#     print('filtered results: ', filtered_results[:3])
-#     with open('recommended_videos.txt', 'a', encoding='utf-8') as file: 
-#         file.write('\n\nNEW RUN:\n')
-#         file.write(f'Results for: {input}\n\n')
-
-#         file.write(top_filtered_results.__str__())
-        
-#     return top_filtered_results # top 3
-
-
-
-# search_videos('latest advancements in artificial intelligence')
-
-
 from dotenv import load_dotenv
 import os
 from googleapiclient.discovery import build
-import html
 import numpy as np
-print ('before import sentence')
+# print ('before import sentence')
 from sentence_transformers import SentenceTransformer
 
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
-print('after model')
+# print('after model')
 
 # search and then puts the results into a list
 def search_videos(input):
@@ -192,11 +75,37 @@ def search_videos(input):
 
             similarities = np.dot(user_input_normalized, embedded_video_text.T)
 
-            print('video similarity: ', similarities)
+            # print('video similarity: ', similarities)
             # this value generally worked the best
-            if similarities < 0.7:
+            if similarities < 0.4:
                 results_list.remove(video)
             
     return results_list
 
-search_videos('social media')
+# search_videos('social media')
+
+
+# --------- ARCHIVE CODE ----------------
+
+
+#     filtered_results.sort(key=lambda vid: vid['similarity'], reverse=True)
+    
+#     filtered_top3_results = filtered_results[:3]
+
+#     top_filtered_results = {
+#         str(index): video for index, video in enumerate(filtered_top3_results)
+#     }
+   
+#     print('filtered results: ', filtered_results[:3])
+#     with open('recommended_videos.txt', 'a', encoding='utf-8') as file: 
+#         file.write('\n\nNEW RUN:\n')
+#         file.write(f'Results for: {input}\n\n')
+
+#         file.write(top_filtered_results.__str__())
+        
+#     return top_filtered_results # top 3
+
+
+
+# search_videos('latest advancements in artificial intelligence')
+
